c825d823a381e62aff7aae49e7170d32dce2bcf5
aida <aidasyafiqah3@gmail.com>
2023-10-05 12:15:58 +0800

    create form student view

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,60 +1,46 @@
 @extends('layouts.docs')
 
-@section('title', 'Student List')
-@section('page-title', 'Student List')
-@section('action-button')
-    <a href="{{ route('products.create') }}" class="btn btn-primary">Add New Student</a>
-@endsection
+@section('title', 'Student Form')
+
 
 @section('content')
-    <table class="table table-bordered mt-3">
-        <thead>
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Name</th>
-                <th scope="col">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($students as $student)
-                <tr>
-                    <th scope="row">{{ $student->id }}</th>
-                    <td style="white-space: nowrap;">{{ $student->name }}</td>
-                    <td>
-                        <!-- Delete Button Triggering Combined Modal -->
-                        <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal{{ $student->id }}">Delete</button>
+    <div class="card">
+        <h1 class="text-center mt-4">Student Form</h1>
+        <div class="card-body">
+            <form method="POST" action="{{ route('students.store') }}">
+                @csrf
 
-                        <!-- Combined Delete Confirmation Modal -->
-                        <div class="modal fade" id="deleteModal{{ $student->id }}" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="deleteModalLabel">Delete Student</h5>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
+                <div class="mb-3">
+                    <label for="name" class="form-label">Name:</label>
+                    <input type="text" class="form-control" id="name" name="name" required>
+                </div>
 
-                                    <div class="modal-body">
-                                        Are you sure you want to delete this student?
-                                    </div>
+                <div class="mb-3">
+                    <label for="student_id" class="form-label">Student ID:</label>
+                    <input type="text" class="form-control" id="student_id" name="student_id" required>
+                </div>
 
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                                        <form style="display: inline;" action="{{ route('students.destroy', $product->id) }}" method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-danger">Confirm Delete</button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                <div class="mb-3">
+                    <label for="class" class="form-label">Class:</label>
+                    <input type="text" class="form-control" id="class" name="class" required>
+                </div>
 
-                        <a href="{{ route('students.edit', $product->id) }}" class="btn btn-warning">Edit</a>
-                        <a href="{{ route('students.show', $product->id) }}" class="btn btn-info">Show</a>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-@endsection
+                <div class="mb-3">
+                    <label for="course" class="form-label">Course:</label>
+                    <input type="text" class="form-control" id="course" name="course" required>
+                </div>
+
+                <div class="mb-3">
+                    <label for="cgpa" class="form-label">CGPA:</label>
+                    <input type="text" class="form-control" id="cgpa" name="cgpa" required>
+                </div>
+
+                <div class="button-container mb-6">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <a href="{{ route('students.index') }}" class="btn btn-secondary">Back</a>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection>
 
