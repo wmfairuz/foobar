3725fe7b18e6f230262bb7e6385c7d478e89a4cb
rina <rinaartikaalhawari@gmail.com>
2023-10-05 11:53:30 +0800

    update route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+use App\Http\Controllers\PatientController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +30,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('products', BakeryController::class);
+    Route::resource('patients', PatientController::class);
 });
 
 require __DIR__.'/auth.php';
